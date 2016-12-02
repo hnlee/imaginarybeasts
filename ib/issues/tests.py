948321f1c23d202466d8bfaf34f5cfda9dc8e795
Hana Lee <hanalee07@gmail.com>
2016-12-02 14:48:03 -0600

    Write first unit test for home page

--- ib/issues/tests.py
+++ ib/issues/tests.py
@@ -1,1 +1,8 @@
 from django.test import TestCase
+from django.urls import resolve
+from issues.views import home
+
+class HomePageTest(TestCase):
+  def test_root_path_renders_home_view(self):
+    root = resolve('/')
+    self.assertEqual(root.func, home)
